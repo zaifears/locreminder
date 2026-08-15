3eed91c777969e1a10e763fc4df690f114d903b8
zaifears <alshahoriar.hossain@gmail.com>
2026-08-15 18:34:38 +0600

    Fix latlong2 import path (latlong.dart, not latlong2.dart)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 
 import '../models/location_alarm.dart';
 import '../services/alarm_repository.dart';
